3db4c6c7c2339f3e1ee10c1dbfe22f807e93d068
zachabdallah <91718140+zachabdallah@users.noreply.github.com>
2025-04-22 11:30:56 -0800

    Update client.py

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ from vosk import Model, KaldiRecognizer
 RECOGNIZER_RATE = 16000
 FRAMES = 4000
 CHANNELS = 1
-SERVER_URL = "http://127.0.0.1:5050/chat" #hardcoded IP address on my machine
+SERVER_URL = "http://172.16.29.23:5050/chat" #hardcoded IP address on my machine
 #when running client.py on the pi5, get the macs IP address with the command "ipconfig getifaddr en0" in the mac terminal
 #then, replace 127.0.0.1 with the IP address of the mac
 
@@ -25,7 +25,9 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=CHANNELS, rate=RECOGNIZER_RATE, input=True, frames_per_buffer=FRAMES)
 
 def speak(text):
+    stream.stop_stream()
     subprocess.run(["espeak-ng", "-v", "en+f2", "-s", "200", "-a", "200", "-p", "50", text])
+    stream.start_stream()
 
 def listen_and_respond():
     stream.start_stream()
